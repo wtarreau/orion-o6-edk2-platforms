86fa6fc3feeec9a5e0ea2694e7d5d99d06bbcc21
Willy Tarreau <w@1wt.eu>
2025-04-12 10:58:34 +0200

    Platform/O6: restore the OPP values from v2.6
    Some of the very low voltages had been bumped a little bit between
    v2.1 and v2.6: 730->750, 750->790 and 790->800. There might be some
    good stability reasons for this, so let's restore the same values as
    in v2.6 here.

--- Platform/Radxa/Orion/O6/pm_config/user_config.h
+++ Platform/Radxa/Orion/O6/pm_config/user_config.h
@@ -72,14 +72,14 @@ static pm_config_pmic_t pmic_config = {
          *
          * delta_mV is the power rail guard band, +/- Max. 500mV, which will be applied against the built OPP table
          */
-        [DPM_EDP_CPU_LIT] = { .vr_type = VR_MP2845, .pwr_cap =  2500, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 1, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_CPU_GM0] = { .vr_type = VR_MP2845, .pwr_cap =  6500, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 2, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_CPU_GM1] = { .vr_type = VR_MP2845, .pwr_cap =  6500, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 0, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_CPU_GB0] = { .vr_type = VR_MP2845, .pwr_cap =  8000, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 2, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_CPU_GB1] = { .vr_type = VR_MP2845, .pwr_cap =  8000, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 3, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_DSU]     = { .vr_type = VR_MP2845, .pwr_cap =  5500, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 3, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_GPU]     = { .vr_type = VR_MP2845, .pwr_cap = 12000, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 1, .vboot_mV = 750, .delta_mV = 0 },
-        [DPM_EDP_SOC]     = { .vr_type = VR_MP2845, .pwr_cap =  9000, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 0, .vboot_mV = 750, .delta_mV = 0 },
+        [DPM_EDP_CPU_LIT] = { .vr_type = VR_MP2845, .pwr_cap =  2500, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 1, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_CPU_GM0] = { .vr_type = VR_MP2845, .pwr_cap =  6500, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 2, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_CPU_GM1] = { .vr_type = VR_MP2845, .pwr_cap =  6500, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 0, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_CPU_GB0] = { .vr_type = VR_MP2845, .pwr_cap =  8000, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 2, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_CPU_GB1] = { .vr_type = VR_MP2845, .pwr_cap =  8000, .i2c_port = 0, .i2c_addr = 0x45, .i2c_buck = 3, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_DSU]     = { .vr_type = VR_MP2845, .pwr_cap =  5500, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 3, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_GPU]     = { .vr_type = VR_MP2845, .pwr_cap = 12000, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 1, .vboot_mV = 790, .delta_mV = 0 },
+        [DPM_EDP_SOC]     = { .vr_type = VR_MP2845, .pwr_cap =  9000, .i2c_port = 1, .i2c_addr = 0x45, .i2c_buck = 0, .vboot_mV = 790, .delta_mV = 0 },
     },
 };
 

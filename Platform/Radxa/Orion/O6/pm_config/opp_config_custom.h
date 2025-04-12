5947e7c3110f7f3d2b36183e43f13c05149bd4a0
Willy Tarreau <w@1wt.eu>
2025-04-12 19:03:23 +0200

    Platform/O6: increase B1 to 3.2 GHz
    It still looks stable here, at 1030mV after 15mn of builds.
    The temperature reaches 71-72 degrees at peak times.
    
    The config also accepts 3.3 GHz and displays it in the BIOS and
    cpufreq, but it's just 2800 measured, indicating that something
    is capping it to the official limit.

--- Platform/Radxa/Orion/O6/pm_config/opp_config_custom.h
+++ Platform/Radxa/Orion/O6/pm_config/opp_config_custom.h
@@ -74,7 +74,7 @@ static domain_opp_config_t dxs_gb1 = {
         { .level = 2400UL, .voltage = 850 },
         { .level = 2600UL, .voltage = 920 },
         { .level = 2800UL, .voltage = 950 },
-        { .level = 3100UL, .voltage = 990 },   /* sustained */
+        { .level = 3200UL, .voltage = 1030 },   /* sustained */
     },
 };
 
@@ -113,7 +113,7 @@ static domain_opp_config_t dxs_dsu = {
     .sustained_idx = 1,
     .opp_table = {
         { .level =  500UL, .voltage = 790 },
-        { .level = 1300UL, .voltage = 790 },   /* sustained */
+        { .level = 1300UL, .voltage = 830 },   /* sustained */
     },
 };
 
